7d8e4013d49a5a40e14ad057e22885188d86cd8b
Slutishboy <sohomsaha361@gmail.com>
2023-09-22 01:41:54 +0530

    update

--- client/vite-project/src/pages/home.jsx
+++ client/vite-project/src/pages/home.jsx
@@ -16,7 +16,7 @@ useEffect(()=>{
 const fetchPost=async()=>{
 setloading(true);
 try {
-    const response =await fetch('https:ai-image-generator-full-stack.vercel.app/api/v1/post',{
+    const response =await fetch('https://ai-image-generator-full-stack.vercel.app/api/v1/post',{
         method:'GET',
         headers:{
           'Content-Type':'application/json',
